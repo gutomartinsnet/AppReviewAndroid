1df68ba24556055f369790ef1136b6d15e4f30ab
Guto Martins <augusto.ms@gmail.com>
2023-02-15 11:09:14 -0300

    Update AppGetScore
    update code

--- AppGetScore.py
+++ AppGetScore.py
@@ -1,33 +1,35 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.chrome.service import Service
+import requests
+import pandas as pd
+import numpy as np
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-s = Service('C:\\webdrivers\\chromedriver.exe')
-driver = webdriver.Chrome(service=s, options=options)
+#options = webdriver.ChromeOptions()
+#options.add_argument('--headless')
+#s = Service('C:\\webdrivers\\chromedriver.exe')
+#driver = webdriver.Chrome(service=s, options=options)
 
 url_score = 'https://play.google.com/store/apps/details?id=br.gov.sp.prodesp.pptdigital&hl=pt_BR&gl=US'
 
-driver.get(url_score)
+res =  requests.get(url_score)
+#res = requests.get(url)
+html_source = res.text
 soup=BeautifulSoup(html_source, "html.parser")
 
-review_score_src = soup.find('div', {'class','jILTFe'}).text
+review_score_src = soup.find_all('div', {'class','jILTFe'})
 #print(review_score_src)
 
-#review_score_list = []
+review_score_list = []
+for val in review_score_src:
+    try:
+        review_score_list.append(val.get_text())
+    except:
+        pass
+review_score_list[:0]    
 
-#for val in review_score_src:
-#    try:
-#        review_score_list.append(val.get_text())
-#    except:
-#        pass
-#review_score_list[:1]    
-
-g_df = pd.DataFrame(np.array(review_author_list_src),columns=['review_score_src'])
-g_df2 = g_df.join(pd.DataFrame(g_df.pop('review_score_src').tolist()))
-g_df2.head()
-
-g_df2.to_csv('pptdigital-appgoogle-reviews-score.csv',encoding='utf-8-sig')
+g_df = pd.DataFrame(np.array(review_score_list),columns=['review_score_src'])
+print(g_df)
+g_df.to_csv('pptdigital-appgoogle-reviews-score.csv')
 #g_df.to_excel('pptdigital-appgoogle-reviews.xlsx', sheet_name='google play',encoding='utf-8-sig')
